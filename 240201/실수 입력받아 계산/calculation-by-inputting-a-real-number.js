bdd29e04d500a4de172e0d8a79ebe80fce3c7dd1
Youjin <85492452+howyoujini@users.noreply.github.com>
2024-02-01 22:20:32 +0900

    Title: 실수 입력받아 계산, Time: 218ms, Memory: 14MB, Status: Passed - CodeTree

--- 240201/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 240201/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split("\n");
 
-const afterDecimalPoint = 100;
-
 let a = input[0]*100;
 let b = input[1]*100;
 
-let result = (a + b)/afterDecimalPoint;
+let result = (a + b)* 0.01;
 
 console.log(result.toFixed(2));
