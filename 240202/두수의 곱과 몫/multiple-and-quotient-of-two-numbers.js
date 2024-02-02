90c49e012b5de8b51ef18bec925050b9aecf6973
Youjin <85492452+howyoujini@users.noreply.github.com>
2024-02-02 19:28:18 +0900

    Title: 두수의 곱과 몫, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240202/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+function multiSol(x, y){
+    return `${x} * ${y} = ${x*y}`;
+}
+
+function diviSol(x, y){
+    return `${x} / ${y} = ${x/y}`;
+}
+
+console.log(`${multiSol(a, b)}\n${diviSol(a, b)}`);
