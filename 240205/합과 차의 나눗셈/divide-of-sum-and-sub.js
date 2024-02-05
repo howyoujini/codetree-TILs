1a9e8fc9b81ea41b7aa967017f58e40777a38f8b
Youjin <85492452+howyoujini@users.noreply.github.com>
2024-02-05 23:12:40 +0900

    Title: 합과 차의 나눗셈, Time: 224ms, Memory: 14MB, Status: Passed - CodeTree

--- 240205/합과 차의 나눗셈/divide-of-sum-and-sub.js
+++ 240205/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -4,4 +4,4 @@ let input = fs.readFileSync(0).toString().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log((Math.floor((a+b)/(a-b)*100)/100).toFixed(2));
+console.log((Math.floor((a+b)/(a-b)*1000)/1000).toFixed(2));
